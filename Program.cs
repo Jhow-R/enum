570d3c5b41e470a94b47ac00fcfb2f34e5c80a35
Jhonathan Araujo <jonathan.reboucas@unisys.com>
2019-12-10 09:00:01 -0300

    Testando Enums

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestandoEnum.Generos valor;
+
+            valor = TestandoEnum.Generos.Aventura;
+
+            Console.WriteLine(valor);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestandoEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class TestandoEnum
+    {
+        public enum Generos
+        {
+            Aventura,
+            Romance,
+            Suspense,
+            Terror
+        }
+
+        Generos valor = Generos.Aventura;
+    }
+}
